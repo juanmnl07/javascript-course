920eed15a001c2faa387b344b06176a027e65dd9
Juan Manuel Martinez <juan@orbelink.com>
2016-10-04 04:30:45 -0600

    first commit

---
+++ lo-esencial/js/script.js
@@ -0,0 +1,16 @@
+var data = {
+	"estudiantes":[
+		{"codigo":"1", "nombre":"Diego Soto", "nota": 70.0},
+		{"codigo":"2", "nombre":"Luis Delgado", "nota": 83.2},
+		{"codigo":"3", "nombre":"Andrea Salazar", "nota": 97.4},
+		{"codigo":"4", "nombre":"Pedro Alcantara", "nota": 89.3},
+		{"codigo":"5", "nombre":"Sofia Lopez", "nota": 90.5},
+		{"codigo":"6", "nombre":"Amanda Pacheco", "nota": 77.7},
+		{"codigo":"7", "nombre":"Agustin Castro", "nota": 89.0},
+		{"codigo":"8", "nombre":"Juan Vargas", "nota": 78.6},
+		{"codigo":"9", "nombre":"Alonso Mora", "nota": 84.0},
+		{"codigo":"10", "nombre":"Andrey Castillo", "nota": 91.3},
+	]
+}
+
+console.log(data.count());
